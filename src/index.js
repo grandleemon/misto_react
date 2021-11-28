81c4d475be2a1e77bba7c117cc6619bb60c5b372
Max <zeetreex@gmail.com>
2021-11-28 17:48:23 +0200

    basename browser router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Provider} from "react-redux";
 import store from './app/store'
 
 ReactDOM.render(
-    <BrowserRouter >
+    <BrowserRouter basename="/misto_react" >
         <Provider store={store}>
             <App />
         </Provider>
